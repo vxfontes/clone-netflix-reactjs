c1384fe4c4bcbd0f0d6bbd43821d9c5d4a7399b4
Vanessa <nessa1vane@icloud.com>
2021-12-01 15:03:59 -0300

    criando base de backend

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,36 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 
+// rotas provisorias
+app.get('/', (req, res) => {
+    res.json({mensagem: "todos os registros"});
+});
+
+app.get('/:id', (req, res) => {
+    const id = req.params.id;
+    res.json({mensagem: `pegar somente registro com id: ${id}`})
+});
+
+//criar registro
+app.post('/', (req, res) => {
+    const body = req.body;
+    res.json(body);
+});
+
+app.put('/:id', (req, res) => {
+    const id = req.params.id;
+    res.json({mensagem: `atualizar somente registro com id: ${id}`});
+});
+
+app.delete('/:id', (req, res) => {
+    const id = req.params.id;
+    res.json({mensagem: `deletar somente registro com id: ${id}`});
+});
+
+
+//////////////////////////////////////////////////////////
+app.use(morgan('dev'));
 app.listen(8080, () => {
     console.log('Servidor online');
 });
